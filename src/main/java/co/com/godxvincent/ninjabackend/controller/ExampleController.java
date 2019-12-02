77c04a1ec6783c3616b71b5efc9f004c2608efb0
godxvincent <godxvincent@gmail.com>
2019-12-01 20:04:39 -0500

    Learning to pass a complex object

--- src/main/java/co/com/godxvincent/ninjabackend/controller/ExampleController.java
+++ src/main/java/co/com/godxvincent/ninjabackend/controller/ExampleController.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+import co.com.godxvincent.ninjabackend.model.Person;
+
 
 @Controller
 //Aquí empiezo a especificar la ruta de la aplicación 
@@ -19,17 +21,19 @@ public class ExampleController {
 	// @GetMapping("/exampleString") Esta forma estan apartir de las versiones 4.3 
 	@RequestMapping(value="/exampleString", method = RequestMethod.GET)
 	public String exampleString(Model model ) {
+		
 		// Nombre del template sin adicionar al final .html
-		model.addAttribute("name", "Ricardo");
+		model.addAttribute("person", new Person("Ricardo",32));
 		return this.EXAMPLE_VIEW;
 	}
 	
 	// Segunda forma de llamar un template.
 	@RequestMapping(value="/exampleMAV", method = RequestMethod.GET)
 	public ModelAndView exampleMAV() {
+		
 		// Nombre del template sin adicionar al final .html
 		ModelAndView mav = new ModelAndView(this.EXAMPLE_VIEW);
-		mav.addObject("name", "Andres");
+		mav.addObject("person", new Person("Andres",32));
 		return  mav;
 	}
 }
